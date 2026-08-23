6e59d18abcd63bae93f7c65a1078894f87c63430
quangannhat <quangannhat.dev@gmail.com>
2026-08-23 08:59:33 +0700

    d3_p1 done

--- lib/solutions/day_3/day_3.ex
+++ lib/solutions/day_3/day_3.ex
@@ -15,9 +15,23 @@ defmodule Solutions.Day3 do
 
   defp parse_line(line) do
     line
+    |> String.graphemes()
+    |> Enum.map(&String.to_integer/1)
   end
 
-  defp calc_answer(_input) do
-    raise "Day 3 calculation not implemented"
+  defp calc_answer(line) do
+    line
+    |> Enum.map(&calc_line/1)
+    |> Enum.sum()
+  end
+
+  defp calc_line([first | rest]) do
+    Enum.reduce(rest, {0, first}, fn digit, {biggest_number, biggest_digit} ->
+      candidate = biggest_digit * 10 + digit
+      new_max = max(candidate, biggest_number)
+      new_biggest_digit = max(biggest_digit, digit)
+      {new_max, new_biggest_digit}
+    end)
+    |> elem(0)
   end
 end
